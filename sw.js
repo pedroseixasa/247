17b7a83e2b7b5c704cf031b0a0b3715100d78bee
pedro <pedroseixasa@gmail.com>
2026-04-01 05:39:25 +0100

    Final: proportional consistent padding-bottom on service-info mobile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: Network-first strategy for all requests
-const CACHE_NAME = "247barbearia-v12";
+const CACHE_NAME = "247barbearia-v13";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
