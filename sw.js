6d2ba8140009814bfa2cb9cc4d93e905c740fb28
pedro <pedroseixasa@gmail.com>
2026-04-01 17:31:31 +0100

    Remove unused Showcase Cards section from admin panel

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: Network-first strategy for all requests
-const CACHE_NAME = "247barbearia-v20";
+const CACHE_NAME = "247barbearia-v21";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
