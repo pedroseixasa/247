bab5f7793037dcd2ef05da2b0f8c6d36ad19cf86
pedro <pedroseixasa@gmail.com>
2026-04-01 05:19:28 +0100

    Fix service cards layout: mobile column flex with centered title/price, desktop btn 95% width

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: Network-first strategy for all requests
-const CACHE_NAME = "247barbearia-v5";
+const CACHE_NAME = "247barbearia-v6";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
