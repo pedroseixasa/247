0cf35d60e43965d8ff307b2337babd39e097b1ca
pedro <pedroseixasa@gmail.com>
2026-04-01 17:38:42 +0100

    Create .section-intro class with responsive margins - 7rem desktop, 2rem mobile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: Network-first strategy for all requests
-const CACHE_NAME = "247barbearia-v21";
+const CACHE_NAME = "247barbearia-v22";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
