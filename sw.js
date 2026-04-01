98b15055a607af4d85467048220b99cdbfd36db2
pedro <pedroseixasa@gmail.com>
2026-04-01 17:57:50 +0100

    Improve MARCAR button styling for tablet - larger padding, font-size, and hover effects

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: Network-first strategy for all requests
-const CACHE_NAME = "247barbearia-v23";
+const CACHE_NAME = "247barbearia-v24";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
